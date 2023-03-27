dfe54a06162135d4bd405cd6a0ebeedd1915d4c5
Tryd0g0lik <work80@mail.ru>
2023-03-27 16:07:12 +0700

    'wabpack' package reinstalled

--- webpack.config.js
+++ webpack.config.js
@@ -1,44 +1,60 @@
-const path = require('path');
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// Generated using webpack-cli https://github.com/webpack/webpack-cli
 
-module.exports = {
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// const isProduction = process.env.NODE_ENV == "production";
+
+const stylesHandler = isProduction
+	? MiniCssExtractPlugin.loader
+	: "style-loader";
+
+moduls.exports = {
+	entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
+		path: path.resolve(__dirname, "dist"),
   },
+	devServer: {
+		open: true,
+		host: "localhost",
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: "index.html",
+		}),
+
+		// Add your plugins here
+		// Learn more about plugins from https://webpack.js.org/configuration/plugins/
+	],
   module: {
     rules: [
       {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: 'babel-loader',
-        },
+				test: /\.(js|jsx)$/i,
+				loader: "babel-loader",
       },
       {
-        test: /\.html$/,
-        use: [
-          {
-            loader: 'html-loader',
-          },
-        ],
+				test: /\.css$/i,
+				use: [stylesHandler, "css-loader", "postcss-loader"],
       },
       {
-        test: /\.css$/,
-        use: [
-          MiniCssExtractPlugin.loader, 'css-loader',
-        ],
+				test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
+				type: "asset",
       },
+
+      // Add your rules for custom modules here
+      // Learn more about loaders from https://webpack.js.org/loaders/
     ],
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: './src/index.html',
-      filename: './index.html',
-    }),
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-      chunkFilename: '[id].css',
-    }),
-  ],
+	},
 };
+
+// module.exports = () => {
+//   if (isProduction) {
+//     config.mode = "production";
+
+//     config.plugins.push(new MiniCssExtractPlugin());
+//   } else {
+//     config.mode = "development";
+//   }
+//   return config;
+// };
